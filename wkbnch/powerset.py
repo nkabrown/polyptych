7e08620358622210a3a4e636cb426749c854730e
Nathan <nathan.brown@annalect.com>
2020-03-21 16:18:21 -0400

    Follow PEP 257 docstring conventions

--- wkbnch/powerset.py
+++ wkbnch/powerset.py
@@ -1,9 +1,14 @@
-"""Power set := the set of all subsets of a set."""
+"""Power set is the set of all subsets of a set.
 
+S = {x, y, z}
+
+P(S) = {{}, {x}, {y}, {z}, {x, y}, {x, z}, {y, z}, {x, y, z}}
+"""
 from itertools import chain, combinations
 
-"""Return the list of all subsets as a list of tuples of an iterable."""
+
 def powerset(iterable):
+   """Return the list of all subsets as a list of tuples of an iterable."""
     S = list(iterable)
     # combinations: r-length tuples, in sorted order, no repeated elements
     return list(chain.from_iterable(combinations(S, r) for r in range(len(S) + 1)))
